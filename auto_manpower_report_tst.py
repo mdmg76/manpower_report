eca9598e696aed399f2c76794c32317ce176dfef
MDMG <56464732+mdmg76@users.noreply.github.com>
2024-09-13 04:55:43 +0000

    Update auto_manpower_report_tst.py
    this uses .env

--- auto_manpower_report_tst.py
+++ auto_manpower_report_tst.py
@@ -1,5 +1,4 @@
 #!python
-# auto_sotrovimab_reporter.py automatically sends the daily sotrovimab report to all assigned recipients.
 
 import os
 from pathlib import Path
@@ -17,7 +16,7 @@ today = datetime.today().strftime('%d-%m-%Y')
 sheet_date = datetime.today().strftime('%d')
 sheet_month = datetime.today().strftime('%b')
 # '''
-# report_loc = Path('//rah-fileserver/Users/Rahba Shared Folder/Pharmacy/Pharmacy Reports/ARH Pharmacy Daily Manpower Updates/')
+# report_loc = Path('//network/path/to/Daily Manpower Updates/')
 # report_fil = [os.path.basename(name) for name in list(report_loc.glob(f'* {sheet_month} *'))][0]
 # month_wb = openpyxl.load_workbook(report_loc / report_fil)
 # for day in month_wb.sheetnames:
@@ -26,7 +25,7 @@ sheet_month = datetime.today().strftime('%b')
 #         month_wb.save(report_fil)
 
 credentials = Credentials(my_user, my_pass)
-account = Account('PharmacyDeptRAH@seha.ae', credentials=credentials, autodiscover=True)
+account = Account('sender email', credentials=credentials, autodiscover=True)
 
 m = Message(
     account=account,
@@ -35,15 +34,15 @@ m = Message(
     <html>
         <body style="font-family:Segoe UI; color:#228B99">
             <p>Good Morning Sara,</p>
-            <p>Kindly find Al Rahba manpower report for today {today}.</p>
+            <p>Kindly find <funtion name> manpower report for today {today}.</p>
             <p>Regards,</p>
         </body>
     </html>
     '''.format(today=today)),
-    to_recipients=[Mailbox(email_address='mmughazi@thelifecorner.ae')
+    to_recipients=[Mailbox(email_address='recipient email')
     ],
 
-    cc_recipients=['mmughazi@thelifecorner.ae', 'mmughazi@thelifecorner.ae'], 
+    cc_recipients=['cc1 email', 'cc2 email'], 
 )
 # my_file = FileAttachment(name=report_fil, content=open(report_fil, 'rb').read())
 # m.attach(my_file)
