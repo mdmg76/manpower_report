74c78b76f954dde0349349977a05817e4a648f40
MDMG <56464732+mdmg76@users.noreply.github.com>
2024-09-13 04:53:04 +0000

    Update auto_manpower_report.py

--- auto_manpower_report.py
+++ auto_manpower_report.py
@@ -1,5 +1,5 @@
 #!python
-# auto_sotrovimab_reporter.py automatically sends the daily sotrovimab report to all assigned recipients.
+
 
 import os
 from pathlib import Path
@@ -11,7 +11,7 @@ today = datetime.today().strftime('%d-%m-%Y')
 sheet_date = datetime.today().strftime('%d')
 sheet_month = datetime.today().strftime('%b')
 # '''
-report_loc = Path('//rah-fileserver/Users/Rahba Shared Folder/Pharmacy/Pharmacy Reports/ARH Pharmacy Daily Manpower Updates/')
+report_loc = Path('//network/path/to/Daily Manpower Updates/') #change to actual path
 report_fil = [os.path.basename(name) for name in list(report_loc.glob(f'* {sheet_month} *'))][0]
 month_wb = openpyxl.load_workbook(report_loc / report_fil)
 for day in month_wb.sheetnames:
@@ -19,8 +19,8 @@ for day in month_wb.sheetnames:
         month_wb.remove(month_wb[day])
         month_wb.save(report_fil)
 
-credentials = Credentials('mmughazi@thelifecorner.ae', '114622@PharmD')
-account = Account('mmughazi@thelifecorner.ae', credentials=credentials, autodiscover=True)
+credentials = Credentials('your email', 'ypur email password') # better to udpate to use .env
+account = Account('your email', credentials=credentials, autodiscover=True)
 
 m = Message(
     account=account,
@@ -29,15 +29,15 @@ m = Message(
     <html>
         <body style="font-family:Segoe UI; color:#228B99">
             <p>Good Morning Sara,</p>
-            <p>Kindly find Al Rahba manpower report for today {today}.</p>
+            <p>Kindly find <function name> manpower report for today {today}.</p>
             <p>Regards,</p>
         </body>
     </html>
     '''.format(today=today)),
-    to_recipients=[Mailbox(email_address='sjjaberi@thelifecorner.ae')
+    to_recipients=[Mailbox(email_address='recipient email')
     ],
 
-    cc_recipients=['maltunaiji@thelifecorner.ae', 'msyed@thelifecorner.ae'], 
+    cc_recipients=['cc1 email', 'cc2 email'], 
 )
 my_file = FileAttachment(name=report_fil, content=open(report_fil, 'rb').read())
 m.attach(my_file)
